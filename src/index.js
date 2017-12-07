9f9c5eff54955fccc30826dd1645b6f313e81aad
Jannis R <mail@jannisr.de>
2017-12-07 17:36:16 +0100

    poll UI, part 5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,57 @@
 'use strict'
 
-;(async function () {
+const snabbdom = require('snabbdom')
+const attrsForSnabbdom = require('snabbdom/modules/attributes').default
+const propsForSnabbdom = require('snabbdom/modules/props').default
+const classForSnabbdom = require('snabbdom/modules/class').default
+const styleForSnabbdom = require('snabbdom/modules/style').default
+const eventsForSnabbdom = require('snabbdom/modules/eventlisteners').default
+const randomId = require('crypto-random-string')
+const h = require('snabbdom/h').default
+
+const renderPoll = require('./ui/poll')
+
+const patch = snabbdom.init([
+	attrsForSnabbdom,
+	propsForSnabbdom,
+	classForSnabbdom,
+	styleForSnabbdom,
+	eventsForSnabbdom,
+])
+
+;(async () => {
 	const baseUrl = global.location.protocol + '//' + global.location.host
 	const archive = new DatArchive(baseUrl)
 
-	// todo
+	const poll = JSON.parse(await archive.readFile('/poll.json'))
+	const votes = []
+	for (let file of await archive.readdir('/votes')) {
+		const vote = JSON.parse(await archive.readFile('/votes/' + file))
+		votes.push(vote)
+	}
+
+	const addVote = async (vote) => {
+		// todo: validation
+		const dest = '/votes/' + vote.id + '.json'
+		await archive.writeFile(dest, JSON.stringify(vote))
+		votes.push(vote)
+	}
+
+	const onSubmit = (author, chosen) => {
+		addVote({
+			id: randomId(8),
+			author,
+			choices: Object.keys(chosen).map((choiceId) => {
+				return {choiceId, value: chosen[choiceId]}
+			})
+		})
+		.catch(console.error)
+	}
+
+	const tree = h('main', {attrs: {id: 'content'}}, [
+		renderPoll(poll, votes, onSubmit)
+	])
+	const root = document.querySelector('#app')
+	patch(root, tree)
 })()
 .catch(console.error)
